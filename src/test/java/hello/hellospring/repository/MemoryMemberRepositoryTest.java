b5963d3a22fe0923c79cea53a29d6a31ae7f47a7
Han JiHo <h2j970313@gmail.com>
2023-09-28 00:14:52 +0900

    add: test클래스 생성하고 테스트 해보기 @AfterEach어노테이션 공부

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+    
+    // @AfterEach: 하나의 테스트가 끝날 때마다 실행될 메소드를 지정한다.
+    //  - 테스트는 각각 독립적으로 실행되어야 한다. 그렇지 않을 경우 전 테스트에서 사용된 데이터가 남아있어,
+    //    정상적인 테스트가 어려울 수 있기 때문이다. 하나의 테스트가 끝날 때마다 이전 데이터를 지우는 메소드를 등록하자.
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
